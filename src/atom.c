e0f8bc656dc8cd960161caf8db9fa7a581fce553
limbo <limbo_china@163.com>
2017-05-01 04:54:36 +0800

    update

--- src/atom.c
+++ src/atom.c
@@ -254,8 +254,8 @@ void adjustAtoms(struct SystemStr* sys){
         if(getMyRank()==2){
             // for(int i=0;i<6;i++)
             //     printf("%d \n",sys->datacomm->neighborProc[i] );
-            printf("%d \n",(dimen%2)?-1:1 );
-            printf("n_dimen:%d neighbor:%d\n",n_dimen,neighbor);
+            printf("(dimen%2)?-1:1---%d \n",(dimen%2)?-1:1 );
+            printf("dimen:%d n_dimen:%d neighbor:%d\n",dimen,n_dimen,neighbor);
         }
 
         int PutSize=0;
