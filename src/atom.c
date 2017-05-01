9b53ac28613157dff6aec44e58441d91ddfaa5ca
limbo <limbo_china@163.com>
2017-05-01 15:40:37 +0800

    update

--- src/atom.c
+++ src/atom.c
@@ -267,7 +267,7 @@ void adjustAtoms(struct SystemStr* sys){
         beginTimer(test);
         MPI_Win_allocate_shared(PutSize*sizeof(AtomData), sizeof(char),
          MPI_INFO_NULL,MPI_COMM_WORLD, &PutBuf, &win);
-
+        endTimer(test);
 
         ///应该拿到外面去，不能每次都开辟一个新窗口，从头到尾都用同一个
 
@@ -284,7 +284,7 @@ void adjustAtoms(struct SystemStr* sys){
         // }
 
         // 调用mpi_sendrecv函数，与邻居进程发送与接收原子数据
-        endTimer(test);
+        
         MPI_Win_shared_query(win,neighbor, &recv, &t, &getbuf);
         //printf("%d \n",recv );
 
